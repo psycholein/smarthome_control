3af2ac93ca792f0e7763f0537f21a4165a11a5c7
Psycho <priebeman@googlemail.com>
2014-12-14 14:43:38 +0100

    change webserver lib

--- libs/webserver.py
+++ libs/webserver.py
@@ -1,20 +1,89 @@
-from flask import Flask
-from multiprocessing import Process
-app = Flask(__name__)
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import threading, os
 
-class Webserver:
+class IndexHandler(tornado.web.RequestHandler):
+  @tornado.web.asynchronous
+  def get(self):
+    self.render('index.html')
 
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+  def initialize(self, clients, dispatcher):
+    self.clients    = clients
+    self.dispatcher = dispatcher
+
+  def open(self, *args):
+    self.clients.add(self)
+    self.dispatcher.client_connected(self)
+    self.stream.set_nodelay(True)
+
+  def on_message(self, message):
+    self.dispatcher.send(message, self)
+
+  def on_close(self):
+    self.clients.remove(self)
+    self.dispatcher.client_disconnected(self)
+
+
+class Clients:
   def __init__(self):
-    global app
-    self.server = Process(target=app.run, args=('0.0.0.0', 3000,))
+    self.clients = []
 
-  @app.route("/")
-  def hello(self):
-    return "Hello World!"
+  def add(self, client):
+    self.clients.append(client)
 
-  def start(self):
-    self.server.start()
+  def remove(self, client):
+    self.clients.remove(client)
+
+  def get(self):
+    return self.clients
+
+  def clear(self):
+    self.clients = []
+
+
+class Webserver(threading.Thread):
+  def __init__(self, dispatcher = None):
+    super(self.__class__, self).__init__()
+
+    static_path     = os.path.dirname(__file__)+'/../web/static'
+    template_path   = os.path.dirname(__file__)+'/../web/views'
+
+    self.dispatcher = dispatcher
+    self.clients    = Clients()
+    self.running    = False
+    self.app        = tornado.web.Application([
+      (r'/ws', WebSocketHandler, {
+                  "clients"       : self.clients,
+                  "dispatcher"    : self.dispatcher
+               }),
+      (r'/', IndexHandler),
+    ],
+    static_path   = static_path,
+    template_path = template_path,
+    autoreload    = True)
+
+  def run(self):
+    self.running = True
+    self.app.listen(3000)
+    tornado.ioloop.IOLoop.instance().start()
+
+    self.running = False
+    self.clients.clear()
 
   def stop(self):
-    self.server.terminate()
-    self.server.join()
+    if not self.running: return
+
+    ioloop = tornado.ioloop.IOLoop.instance()
+    ioloop.add_callback(lambda x: x.stop(), ioloop)
+    self.join()
+
+  def send(self, data):
+    if not self.running: return
+
+    for client in self.clients.get():
+      client.write_message(data)
+
+  def is_running(self):
+    return self.running
