8767cb7b63cffbdef5347c3661ccbe6ad4317475
psycho <priebeman@googlemail.com>
2016-03-07 12:59:29 +0100

    debug

--- classes/api.py
+++ classes/api.py
@@ -2,8 +2,8 @@ class Api:
   def sensor(self, values):
     device = values.get('device')
     value  = values.get('value')
-    if not device or not value: return
     print device, value, "\n\n"
+    if not device or not value: return
 
     self.values.addValue(device, 'humidity', value)
     self.values.addValue(device, 'device', device)
